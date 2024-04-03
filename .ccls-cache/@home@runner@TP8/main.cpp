dd31d56b532e83ed34cf42fe273d49d56771fba1
peppapig1911 <158774869+peppapig1911@users.noreply.github.com>
2024-04-03 07:42:58 +0000

    Création du plateau

--- .ccls-cache/@home@runner@TP8/main.cpp
+++ .ccls-cache/@home@runner@TP8/main.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+// Fonction pour afficher le plateau
+void afficherPlateau(const vector<vector<char>>& plateau) {
+    for (const auto& ligne : plateau) {
+        for (char casePlateau : ligne) {
+            cout << "|_" << casePlateau << "";
+        }
+        cout << "|" << endl;
+    }
+}
+
+int main() {
+    // Taille du plateau
+    int x, y;
+    cout << "Entrez le nombre de lignes (x) : ";
+    cin >> x;
+    cout << "Entrez le nombre de colonnes (y) : ";
+    cin >> y;
+
+    // Initialisation du plateau
+    vector<vector<char>> plateau(x, vector<char>(y, '_'));
+
+    // Affichage du plateau initial
+    cout << "Plateau initial :" << endl;
+    afficherPlateau(plateau);
+
+    return 0;
+}
